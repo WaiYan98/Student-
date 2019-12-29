0258dc43d733deb8986a735cd85b0034f51b20a7
WaiYan98 <wyh.waiyan98@gmail.com>
2019-12-29 18:59:54 +0630

    uncomment test class

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,11 @@
-//public class Test {
-//
-////    public static void main(String[] args) {
-////        int id = 0;
-////
-////        AdminUserFunction ad = new AdminUserFunction();
-////        ad.readScore();
-////        ad.readLogin();
-////        System.out.println(ad.checkFinishQuiz(ad.nameToId("sohai")));
-////
-////    }
-////}
+public class Test {
+
+    public static void main(String[] args) {
+        int id = 0;
+
+        AdminUserFunction ad = new AdminUserFunction();
+
+        System.out.println("Test");
+
+    }
+}
